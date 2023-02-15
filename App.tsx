03c24b9cd2336758fefde5369c51dff4f8162cf9
nbc7 <nathanborgescastro@gmail.com>
2023-02-15 11:09:46 -0300

    added navigation

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import './src/lib/dayjs';
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screen/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold });
@@ -14,7 +14,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </>
   );
