e2f03c44ce5a0593a9567c55fe8b9e5dd5a6335e
nbc7 <nathanborgescastro@gmail.com>
2023-02-15 15:24:20 -0300

    fixed border style on text input box

--- src/screen/New.tsx
+++ src/screen/New.tsx
@@ -29,7 +29,7 @@ export function New() {
         <Text className="mt-6 text-white font-semibold text-base">Qual seu comprometimento?</Text>
 
         <TextInput
-          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
         ></TextInput>
